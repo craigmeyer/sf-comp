b31f646658fecf15de03309f61bc41a9503fab54
craigmeyer <79262891+craigmeyer@users.noreply.github.com>
2021-06-28 09:08:54 +0800

    Added new config class

--- ReadConfig.py
+++ ReadConfig.py
@@ -1,6 +1,6 @@
 
 
-class FinderOptions(object):
+class CrossmatchOptions(object):
 
 	def __init__(self, ref, target, filename):
 
@@ -66,7 +66,71 @@ class FinderOptions(object):
 
 				# Handle invalid configuration option
 				else:
+					raise OSError('Invalid config option passed: %s. Please only use options in the sample config file.' % line[0])
+
+
+
+
+
+class AnalysisOptions(object):
+
+	def __init__(self, filename):
+
+		# Set default values
+		self.snap = 0
+		self.h = 0.678
+		self.boxsize = 0
+		self.snapdir = ''
+		self.SFdir = ''
+		self.VELdir = ''
+
+		with open(filename, 'r') as f:
+
+			for line in f:
+
+				# This line is a comment - skip it
+				if line[0] == '#':
+					continue
+
+				# Remove all spaces in the line, including leading and trailing whitespace
+				line = line.replace(' ', '')
+				line = line.strip()
+
+				if not line:
+					continue
+
+				# Separate the line into two elements either sign of the assignment operator
+				# First element is name of option, second element is the value
+				line = line.split('=')
+
+				if line[0] == 'snap':
+					self.snap = int(line[1])
+				elif line[0] == 'h':
+					self.h = float(line[1])
+				elif line[0] == 'boxsize':
+					self.boxsize = float(line[1])
+				elif line[0] == 'snapdir':
+					self.snapdir = line[1]
+
+				# SubFind specific options
+				elif line[0] == 'SFdir':
+					self.SFdir = line[1]
+
+				# VELOCIraptor specific options
+				elif line[0] == 'VELdir':
+					self.VELdir = line[1]
+
+				# Handle invalid configuration option
+				else:
 					raise OSerror('Invalid config option passed: %s. Please only use options in the sample config file.' % line[0])
+
+
+
+
+
+
+
+
 
 
 
